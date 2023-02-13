eb6bd23226ca953d3d609ae0d0ee49473be0fa51
Raven-15 <piedrahitaangelica@gmail.com>
2023-02-13 14:26:18 -0500

    Taller 1

---
+++ Taller 1/Solucion Punto#1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int firstInt, secondInt, result;
+	printf("favor ingresar el primer numero entero: ");
+	scanf("%d" , &firstInt);
+	
+	printf("por favor ingresar el segundo entero: ");
+	scanf("%d" , &secondInt);
+	
+	//result=firsInt+secondInt;
+	printf("el resultado de esta suma es %d" , firstInt+secondInt);
+	
+	return 0;
+}
+
