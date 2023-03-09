cd8a338aa3daf621727f390b6f3177da113a6c19
Raven-15 <piedrahitaangelica@gmail.com>
2023-03-09 12:36:46 -0500

    progra

---
+++ Exposicion 9 Marzo/Exposición sobre una función de Library.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x = 2.5;
+    double resultado = sinh(x);
+    
+    printf("El seno hiperbolico de %f es %f\n", x, resultado);
+    
+    return 0;
+}
+
