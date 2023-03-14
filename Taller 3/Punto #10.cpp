7371d87488c9a02352e786f7d1aa4851929f2554
Raven-15 <piedrahitaangelica@gmail.com>
2023-03-14 15:10:14 -0500

    progra

---
+++ Taller 3/Punto #10.cpp
@@ -0,0 +1,31 @@
+//Punto #10
+
+#include <stdio.h>
+#include <string.h>
+
+void encriptar(char cadena[]);
+
+int main() {
+    char cadena[100];
+
+    printf("Ingrese una cadena de caracteres: ");
+    scanf("%s", cadena);
+
+    printf("La cadena encriptada es: ");
+    encriptar(cadena);
+    printf("%s\n", cadena);
+
+    return 0;
+}
+
+void encriptar(char cadena[]) {
+    int longitud = strlen(cadena);
+    for (int i = 0; i < longitud; i++) {
+        if (cadena[i] >= 'a' && cadena[i] <= 'z') {
+            cadena[i] = 'a' + ((cadena[i] - 'a' + 13) % 26);
+        } else if (cadena[i] >= 'A' && cadena[i] <= 'Z') {
+            cadena[i] = 'A' + ((cadena[i] - 'A' + 13) % 26);
+        }
+    }
+}
+
