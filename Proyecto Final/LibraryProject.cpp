eabc36a4f603f8e9aa9fe8b031854414f8d5100d
Raven-15 <piedrahitaangelica@gmail.com>
2023-05-27 09:55:50 -0500

    progra

--- Proyecto Final/LibraryProject.cpp
+++ Proyecto Final/LibraryProject.cpp
@@ -1,45 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "biblioteca.h"
+#include "LibraryProject.h"
+
+#define MAX_LIBROS 200 
+#define MAX_TITULO 100 
+#define MAX_AUTOR 50 
+
+FILE *arc1;
+FILE *arc2;
 
 
-
-int main() {
+typedef struct {
+	bool is_valid;
+	char titulo[MAX_TITULO];
+	char autor[MAX_AUTOR];
+	bool disponible;
+	int numPag;
 	
-	struct Usuario usuarios[MAX_USUARIOS];
-    int numeroUsuarios = 0;
+}libro;
+
+void agregarLibro() {
 	
-    struct Libro biblioteca[MAX_LIBROS];
-    int numeroLibros = 0;
+	libro library;
+	printf("Ingrese el t%ctulo del libro: ", 161);
+	fflush(stdin);
+	gets(library.titulo);
+	printf("Ingrese el autor del libro: ");
+	gets(library.autor);
+	fflush(stdin);
+	printf("Ingrese el n%cmero de p%cginas: ", 160);
+	scanf("%d", &library.numPag);
+	
+	library.is_valid = true;
+	library.disponible = true;
 
+	arc1 = fopen("libro.txt", "a");
+	if(arc1 == NULL) {
+		printf("Error al abrir el archivo.\n");
+		exit(EXIT_FAILURE);
+	}else {
+		fwrite(&library, sizeof(library), 1, arc1);
+		fclose(arc1);
+		arc1 = NULL;
+	}
+	printf("T%ctulo: %s\n", 161, library.titulo);
+	printf("Autor: %s\n", library.autor);
+	printf("N%cmero de p%cginas: %d\n", 163, 160, library.numPag);
+}
 
-    agregarLibro(biblioteca, &numeroLibros, 1, "Del Amor y Otros Demonios", "Gabriel Garcia Marquez", 1994);
-    agregarLibro(biblioteca, &numeroLibros, 2, "Ana Frank", "Melissa Muller", 1998);
-    agregarLibro(biblioteca, &numeroLibros, 3, "Don Quijote de la Mancha", "Miguel de Cervantes", 1604 - 1605);
+void mostrarBiblioteca() {
+	libro biblioteca[MAX_LIBROS];
+	int numeroLibros = 0;
+	
+	arc2 = fopen("libro.txt", "r");
+	
+	if(arc2 == NULL) {
+		printf("Error al abrir el archivo.\n");
+		exit(EXIT_FAILURE);
+	}else {
+		while(fread(&biblioteca[numeroLibros], sizeof(libro), 1, arc2) == 1) {
+			numeroLibros++;
+		}
+		fclose(arc2);
+		arc2 = NULL;
+	}
+	printf("La casa del Libro:\n");
+	for(int j = 0; j < numeroLibros; j++) {
+		printf("Autor: %s\n", biblioteca[j].autor);
+		printf("T%ctulo: %s\n", 161, biblioteca[j].titulo);
+		printf("N%cmero de p%cginas:%d\n", 163, 160, biblioteca[j].numPag);
+		printf("\n-------------------------------\n");
+	}
+}
 
-    agregarUsuario(usuarios, &numeroUsuarios, 1, "Angelica Piedrahita");
-    agregarUsuario(usuarios, &numeroUsuarios, 2, "Sara A. Caballero");
-    agregarUsuario(usuarios, &numeroUsuarios, 2, "Francisco. C");
+const char* boolToString(bool value) {
+    return value ? "Si" : "No";
+}
 
-    prestarLibro(biblioteca, numeroLibros, 1, usuarios, numeroUsuarios);
-    prestarLibro(biblioteca, numeroLibros, 2, usuarios, numeroUsuarios);
-    prestarLibro(biblioteca, numeroLibros, 3, usuarios, numeroUsuarios);
+char tuper(char letter){
+    if(letter >= 97 && letter <= 122)
+        letter = letter - 32;
+    return letter;
+}
 
-    devolverLibro(biblioteca, numeroLibros, 1, usuarios, numeroUsuarios);
-
-
-    mostrarBiblioteca(biblioteca, numeroLibros);
-    mostrarUsuarios(usuarios, numeroUsuarios);
-
-    guardarBiblioteca(biblioteca, numeroLibros, "biblioteca.dat");
-    guardarUsuarios(usuarios, numeroUsuarios, "usuarios.dat");
-
-    cargarBiblioteca(biblioteca, &numeroLibros, "biblioteca.dat");
-    cargarUsuarios(usuarios, &numeroUsuarios, "usuarios.dat");
-
-    mostrarBiblioteca(biblioteca, numeroLibros);
-    mostrarUsuarios(usuarios, numeroUsuarios);
-
-    return 0;
+int menu () {
+	
+	int opcion;
+	printf("\n LA CASA DEL LIBRO\n\n");
+	printf("1. Agregar libro\n");
+	printf("2. Mostrar biblioteca\n");
+	printf("3. Buscar libro\n");
+	printf("4. Prestar libro\n");
+	printf("5. Devolver libro\n");
+	printf("6. Agregar usuario\n");
+	printf("7. Buscar usuarios\n");
+	printf("8. Eliminar usuario\n");
+	printf("9. Salir\n");
+	printf("INGRESE UNA OPCION ENTRE 1 Y 11: ");
+	fflush(stdin);
+	scanf("%d", &opcion);
+	
+	return opcion;
 }
--- Proyecto Final/LibraryProject.h
+++ Proyecto Final/LibraryProject.h
@@ -1,35 +1,20 @@
-#ifndef BIBLIOTECA_H
-#define BIBLIOTECA_H
+#ifndef LIBRARYPROJECT_H
+#define LIBRARYPROJECT_H
 
-#define MAX_LIBROS 200
-#define MAX_TITULO 200
-#define MAX_AUTOR 100
-#define MAX_USUARIOS 200
-
-struct Libro {
-	int codigo;
-	anioPublicacion;
-	int disponible;
-	char titulo [MAX_TITULO];
-	char autor [MAX_AUTOR];
-};
-
-struct Usuario {
-    int cedula;
-    char nombre[MAX_AUTOR];
-};
-
-void agregarLibro(struct Libro *biblioteca, int *numeroLibros, int codigo, const char *titulo, const char *autor, int anioPublicacion);
-void mostrarBiblioteca(struct Libro *biblioteca, int numeroLibros);
-void buscarLibro(struct Libro *biblioteca, int numeroLibros, const char *titulo);
-void prestarLibro(struct Libro *biblioteca, int numeroLibros, int codigo, struct Usuario *usuarios, int numUsuarios);
-void devolverLibro(struct Libro *biblioteca, int numeroLibros, int codigo);
-void agregarUsuario(struct Usuario *usuarios, int *numeroUsuarios, int id, const char *nombre);
-void mostrarUsuarios(struct Usuario *usuarios, int numeroUsuarios);
-void guardarBiblioteca(struct Libro *biblioteca, int numeroLibros, const char *archivo);
-void cargarBiblioteca(struct Libro *biblioteca, int *numeroLibros, const char *archivo);
-void guardarUsuarios(struct Usuario *usuarios, int numeroUsuarios, const char *archivo);
-void cargarUsuarios(struct Usuario *usuarios, int *numeroUsuarios, const char *archivo);
+int menu();
+void agregarLibro();
+void mostrarBiblioteca();
+const char* boolToString(bool value);
+char tuper(char letter);
+//void buscarLibro();
+/*void prestarLibro();
+void devolverLibro();
+void agregarUsuario();
+void mostrarUsuarios();
+void guardarBiblioteca();
+void cargarBiblioteca();
+void guardarUsuarios();
+void cargarUsuarios();*/
 
 #endif
 
